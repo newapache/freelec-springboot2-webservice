97a570e916191ebf6675db4e85d3c4519c69d04c
jang-yunmi <newapache@naver.com>
2020-03-02 14:23:02 +0900

    second

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -2,6 +2,8 @@ package com.jojoldu.book.springboot.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
@@ -11,5 +13,11 @@ public class HelloController {
         return "hello";
     }
 
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
 
 }
